8ff31ba3ccaf4af57bdd34213665f08223543192
rcackerman <rebecca@cfa>
2014-08-10 19:20:29 -0400

    Scraper now works with requests. Adding output.

--- do.py
+++ do.py
@@ -2,9 +2,9 @@
 # Gets all pages we need to scrape
 #--------------
 
-import mechanize
+import scrapelib
 from bs4 import BeautifulSoup
-import re, time, csv, sys
+import re, time, csv
 from string import ascii_uppercase
 
 baseurl = 'http://161.11.133.89/ParoleBoardCalendar/interviews.asp?name={letter}&month={month}&year={year}'
@@ -19,8 +19,7 @@ today = time.localtime()
 month_array = [time.localtime(time.mktime([today.tm_year, today.tm_mon + n, 1, 0, 0, 0, 0, 0, 0]))[:2] for n in range(-24, 7)]
 letters = list(ascii_uppercase)
 
-mech = mechanize.Browser()
-# mech.set_handle_robots(False)
+s = scrapelib.Scraper(requests_per_minute=60, retry_attempts=5, retry_wait_seconds=15)
 
 for monthyear in month_array:
   monthvar = str(monthyear[1]).zfill(2)
@@ -31,15 +30,11 @@ for monthyear in month_array:
 ##
 # Cycles through all the urls created
 # by the month, year, and letter combos
-#
-# TODO
-#   * add detail page info for each person
-#   * don't grab the table headers
 
 for url in urls_to_visit:
   print url
-  op = mech.open(url, timeout=5)
-  bs = BeautifulSoup(op.read())
+  op = s.urlopen(url)
+  bs = BeautifulSoup(op)
   # All parolees are within the central table.
   parolee_table = bs.find('table', class_ = "intv")
   # Splitting out into one line per parolee.
@@ -53,28 +48,35 @@ for url in urls_to_visit:
       parolees.append(pl_list)
   except:
     continue
-  time.sleep(1)
 
 for parolee in parolees:
-  print parolee[0:1]
   if not parolee:
     parolees.remove(parolee)
   else:
-    dp = mech.open(detailurl.format(number = parolee[1]), timeout=5)
-    dbs = BeautifulSoup(dp.read())
-    detail_table = dbs.find('table', class_ = "detl")
-    crimes = dbs.find('table', class_ = "intv").find_all('td')
-    for tr in detail_table:
-      detail = tr.getText().split(":")
-      if "nysid" in detail[0].lower() or "name" in detail[0].lower():
+    print detailurl.format(number = parolee[1])
+
+    #checking if the parolee has detail info already
+    if len(parolee) > 10:
+      pass
+    else:
+      try:
+        dp = s.urlopen(detailurl.format(number = parolee[1]), timeout=5)
+        dbs = BeautifulSoup(dp)
+        detail_table = dbs.find('table', class_ = "detl")
+        crimes = dbs.find('table', class_ = "intv").find_all('td')
+        for tr in detail_table:
+          detail = tr.getText().split(":")
+          if "nysid" in detail[0].lower() or "name" in detail[0].lower():
+            continue
+          elif "din" in detail[0].lower():
+            parolee.append(detail[1].strip().replace(u'\xa0', u'')[0:2])
+          else:
+            parolee.append(detail[1].strip().replace(u'\xa0', u''))
+        for c in crimes:
+          parolee.append(c.string.strip())
+      except:
+        print parolee[1] + ' did not work'
         continue
-      elif "din" in detail[0].lower():
-        parolee.append(detail[1].strip().replace(u'\xa0', u'')[0:2])
-      else:
-        parolee.append(detail[1].strip().replace(u'\xa0', u''))
-    for c in crimes:
-      parolee.append(c.string.strip())
-  time.sleep(1)
 
 
 

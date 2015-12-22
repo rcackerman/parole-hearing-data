bf1d2ac88f69353d01cae24df87f6e8643a6317c
John Krauss <irving.krauss@gmail.com>
2015-12-22 13:56:22 -0800

    schedule-able heroku script

---
+++ heroku.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm -rf * # whoa
+rm -f .gitignore
+
+mkdir -p .ssh
+echo "$SSH_PRIVATE_KEY" > .ssh/id_rsa
+chmod 600 .ssh/id_rsa
+echo "$SSH_PUBLIC_KEY" > .ssh/id_rsa.pub
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+
+git init
+git remote add origin git@github.com:talos/parole-hearing-data.git
+git pull origin master
+
+python scrape.py data.csv > output.csv
+
+mv output.csv data.csv
+git add data.csv
+git commit -m 'auto update'
+git push origin master

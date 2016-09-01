12da9fdba6c0e2023d482c142c1957868e9ca6dd
Rebecca <rebecca@casecommons.org>
2016-08-31 21:11:23 -0400

    Updating models to reflect current CSV

--- parole/models/models.py
+++ parole/models/models.py
@@ -8,11 +8,38 @@ class Person(Model):
 
     __tablename__ = 'people'
 
-    din = Column(String, nullable=False)
+    nysid = Column(String)
     birth_date = Column(Date)
     sex = Column(String)
     race_ethnicity = Column(String)
 
+
+class Parolee(Model):
+
+    __tablename__ = 'parolees'
+
+    person_id = Column(Integer, ForeignKey('people.id'), nullable=False, index=True)
+    din = Column(String, nullable=False)
+    year_of_entry = Column(Integer)
+    aggregated_minimum_sentence = Column(String)
+    aggregated_maximum_sentence = Column(String)
+    housing_or_release_facility = Column(String)
+    conditional_release_date = Column(Date)
+    parole_eligibility_date = Column(Date)
+    maximum_expiration_date = Column(Date)
+    parole_maximum_expiration_date = Column(Date)
+    post_release_supervision_me_date = Column(Date)
+    parole_board_discharge_date = Column(Date)
+
+
+class Crimes(Model):
+
+    __tablename__ = 'crimes'
+
+    parolee_id = Column(Integer, ForeignKey('parolees.id'), nullable=False, index=True)
+    crime_of_conviction = Column(String)
+    county_of_commitment = Column(String)
+    class_of_crime = Column(String)
 
 
 class ParoleBoardInterviews(Model):
@@ -20,7 +47,8 @@ class ParoleBoardInterviews(Model):
     __tablename__ = 'parole_board_interviews'
 
     person_id = Column(Integer, ForeignKey('people.id'), nullable=False, index=True)
-    date = Column(Date)
-    type = Column(String)
-    facility = Column(String)
-    decision = Column(String, nullable=True)
+    interview_date = Column(Date)
+    interview_type = Column(String)
+    housing_or_interview_facility = Column(String)
+    release_date = Column(Date)
+    release_type = Column(String)
